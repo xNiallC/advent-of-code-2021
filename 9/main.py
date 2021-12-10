a40b6f80b29ac210edaf22454d63b10ca4c1efe0
Niall Curtis <niallycurtis@gmail.com>
2021-12-10 13:20:36 +0000

    day 9 part 2 so far

--- 9/main.py
+++ 9/main.py
@@ -1,25 +1,50 @@
+class BasinItem:
+  def __init__(self, val):
+    self.val = val
+    self.above = ((0, 0), None)
+    self.right = ((0, 0), None)
+    self.below = ((0, 0), None)
+    self.left = ((0, 0), None)
+
 # With diagonals -> (X-1, Y-1), (X, Y-1), (X+1, Y-1), (X+1, Y), (X+1, Y+1), (X, Y+1), (X-1, Y+1), (X-1, Y)
 # Without diagonals -> (X, Y-1), (X+1, Y), (X, Y+1), (X-1, Y)
-def get_point(adjacent_index, coord, lines):
+def get_point(coord, lines):
   (x_coord, y_coord) = coord
+  try:
+    value = int(lines[y_coord][x_coord])
+  except:
+    return BasinItem(None)
+
+  current_item = BasinItem(value)
+
   is_final_x = (x_coord == (len(lines[y_coord]) - 1))
   is_final_y = (y_coord == (len(lines) - 1))
-  if adjacent_index == 0:
-    if (y_coord == 0):
-      return None
-    return lines[y_coord-1][x_coord]
-  if adjacent_index == 1:
-    if is_final_x:
-      return None
-    return lines[y_coord][x_coord+1]
-  if adjacent_index == 2:
-    if is_final_y:
-      return None
-    return lines[y_coord+1][x_coord]
-  if adjacent_index == 3:
-    if (x_coord == 0):
-      return None
-    return lines[y_coord][x_coord-1]
+
+  adjacent_coord = (x_coord, y_coord-1)
+  if (y_coord == 0):
+    current_item.above = (adjacent_coord, None)
+  else:
+    current_item.above = (adjacent_coord, lines[y_coord-1][x_coord])
+
+  adjacent_coord = (x_coord+1, y_coord)
+  if is_final_x:
+    current_item.right = (adjacent_coord, None)
+  else:
+    current_item.right = (adjacent_coord, lines[y_coord][x_coord+1])
+
+  adjacent_coord = (x_coord, y_coord+1)
+  if is_final_y:
+    current_item.below = (adjacent_coord, None)
+  else:
+    current_item.below = (adjacent_coord, lines[y_coord+1][x_coord])
+
+  adjacent_coord = (x_coord-1, y_coord)
+  if (x_coord == 0):
+    current_item.left = (adjacent_coord, None)
+  else:
+    current_item.left = (adjacent_coord, lines[y_coord][x_coord-1])
+
+  return current_item
 
 def is_low_point(value, adjacents):
   for adjacent in adjacents:
@@ -28,45 +53,41 @@ def is_low_point(value, adjacents):
         return False
   return True
 
-def get_adjacent_points(for_coords, lines):
-  # If the X of a coord is 0, then there is nothing to the left of it
-  # If the X coord is (len(lines[X]) - 1), then there is nothing to the right of it
-  # If the Y coord is 0, there is nothing above it
-  # If the Y coord is (len(lines) - 1), there is nothing below it
-  #
-  # Data is returned as a tuple, with (coord, adjacents)
-  # Adjacents is a list starting from the value above and to the left of the element, going clockwise. If nothing is present, we use None
-  # Diagonals are not included
-  # The length of adjacents is always 4, if not diagonals
-  # Therefore, the first item in the list begins at (X, Y-1)
-  adjacents = []
-  value = int(lines[for_coords[1]][for_coords[0]])
-  for adjacent_item in range(0, 4):
-    adjacents.append(get_point(adjacent_item, for_coords, lines))
-  return {
-    'value': value,
-    'coords': for_coords,
-    'adjacents': adjacents,
-    'is_low_point': is_low_point(value, adjacents),
-    'risk_level': value + 1
-  }
+
+def get_adjacent_points_recursively(current_point, lines, basin_values = [], prev_point = None):
+  # If current point is nothing, well, we do nothing
+  if current_point.val == None:
+    return
+  # If current value is less than it's previous, or previous, doesn't exist, begin tracking basin
+  # Else do nothing and return basin values
+  if prev_point and (current_point.val >= prev_point.val):
+    return
+
+  basin_values.append(current_point.val)
+  print(basin_values)
+
+  # For all children, recurse
+  get_adjacent_points_recursively(get_point(current_point.above[0], lines), lines, basin_values, current_point)
+  get_adjacent_points_recursively(get_point(current_point.right[0], lines), lines, basin_values, current_point)
+  get_adjacent_points_recursively(get_point(current_point.below[0], lines), lines, basin_values, current_point)
+  get_adjacent_points_recursively(get_point(current_point.left[0], lines), lines, basin_values, current_point)
 
 def get_all_adjacent_points(lines):
-  all_adjacents = []
+  all_basins = []
   for y_coord, value in enumerate(lines):
     for x_coord, line in enumerate(value):
-      all_adjacents.append(get_adjacent_points((x_coord, y_coord), lines))
-  return all_adjacents
+      current_point = get_point((x_coord, y_coord), lines)
+      all_basins.append(get_adjacent_points_recursively(current_point, lines))
+      break
+    break
+  return all_basins
 
 def main():
-  with open('./input.txt') as f:
+  with open('./input_test.txt') as f:
     content = f.readlines()
-  content = [x.strip() for x in content]
-  all_adjacent_points = get_all_adjacent_points(content)
-  low_points = [point for point in all_adjacent_points if point['is_low_point']]
-  print(len(low_points))
-  print(low_points)
-  print(sum(item['risk_level'] for item in low_points))
+  content = [[y for y in x.strip()] for x in content]
+  all_basins = get_all_adjacent_points(content)
+  print(all_basins)
 
 if __name__ == '__main__':
   main()
